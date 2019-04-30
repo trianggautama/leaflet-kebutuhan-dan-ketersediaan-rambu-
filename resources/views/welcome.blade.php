e52281bd593d46bf234d73557631145b1f8a5b96
tri angga utama <trianggategarutama@gmail.com>
2019-04-30 12:44:56 +0800

    tambah halaman depan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,321 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="images/dishub.png">
+		<!-- Author Meta -->
+		<meta name="author" content="codepixer">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Dinas Perhubungan</title>
 
-        <title>Laravel</title>
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="depan/css/linearicons.css">
+			<link rel="stylesheet" href="depan/css/font-awesome.min.css">
+			<link rel="stylesheet" href="depan/css/bootstrap.css">
+			<link rel="stylesheet" href="depan/css/magnific-popup.css">
+			<link rel="stylesheet" href="depan/css/nice-select.css">	
+			<link rel="stylesheet" href="depan/css/hexagons.min.css">							
+			<link rel="stylesheet" href="depan/css/animate.min.css">
+			<link rel="stylesheet" href="depan/css/owl.carousel.css">
+			<link rel="stylesheet" href="depan/css/main.css">
+		</head>
+		<body>	
+			  <header id="header" id="home">
+			    <div class="container main-menu">
+			    	<div class="row align-items-center justify-content-between d-flex">
+				      <div id="logo">
+				        <a href="index.html"><img src="images/logo_navbar.png" alt="" title="" /></a>
+				      </div>
+				      <nav id="nav-menu-container">
+				        <ul class="nav-menu">
+				          <li class="menu-active"><a href="index.html">Home</a></li>
+				          <li><a href="about.html">About</a></li>
+				          <li><a href="services.html">Service</a></li>			          
+				          <li><a href="contact.html">Contact</a></li>
+				        </ul>
+				      </nav><!-- #nav-menu-container -->		    		
+			    	</div>
+			    </div>
+			  </header><!-- #header -->
+              <section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-center">
+						<div class="banner-content col-lg-12 col-md-12">
+                            <h5 class="text-uppercase" style="color:white;">Selamat Datang</h5>
+							<h1>
+								Aplikasi Pelaporan Kebutuhan dan Ketersediaan Rambu		
+							</h1>
+							<p class="text-white">
+                            Anda dapat memasukan laporan mengenai lokasi yang dirasa rawan dan dirasa memerluakn rambu lalu lintas untuk mengurangi adanya resiko terjadinya Laka Lalin
+                        </p>
+						</div>												
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+			<!-- Start home-about Area -->
+			<section class="home-about-area section-gap aboutus-about" id="about">
+				<div class="container">
+					<div class="row justify-content-center align-items-center">
+						<div class="col-lg-8 col-md-12 home-about-left">
+							<h6>Brand new app to blow your mind</h6>
+							<h1>
+								We’ve made a life <br>
+								that will change you
+							</h1>
+							<p class="sub">We are here to listen from you deliver exellence</p>
+							<p class="pb-20">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
+							</p>
+							<a class="primary-btn" href="#">Get Started Now</a>
+						</div>
+						<div class="col-lg-4 col-md-12 home-about-right relative">
+							<form class="form-wrap" action="#">
+								<h4 class="text-white pb-20">Request a Quote</h4>
+								<div class="form-select" id="service-select"">
+									<select>
+										<option value="1">Select Service</option>
+										<option value="2">Service One</option>
+										<option value="3">Service Two</option>
+										<option value="4">Service Three</option>
+										<option value="5">Service Four</option>
+									</select>
+								</div>								
+								<input type="text" class="form-control" placeholder="name">
+								<input type="phone" class="form-control" placeholder="Phone Number">
+								<input type="email" class="form-control" placeholder="Email Address">
+								<textarea name="message" id="" cols="30" rows="5" placeholder="Message" class="form-control"></textarea>
+								<button class="primary-btn">Request Free Quote</button>
+							</form>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End home-about Area -->
+				
+			<!-- Start cat Area -->
+			<section class="cat-area section-gap aboutus-cat" id="feature">
+				<div class="container">							
+					<div class="row">
+						<div class="col-lg-4">	
+							<div class="single-cat d-flex flex-column">
+								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-magic-wand"></span></span></a>
+								<h4 class="mb-20" style="margin-top: 23px;">Maintenance</h4>
+								<p>
+									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
+								</p>
+							</div>															
+						</div>
+						<div class="col-lg-4">	
+							<div class="single-cat">
+								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-rocket"></span></span></a>
+								<h4 class="mt-40 mb-20">Residental Service</h4>
+								<p>
+									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
+								</p>
+							</div>															
+						</div>
+						<div class="col-lg-4">
+							<div class="single-cat">
+								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-bug"></span></span></a>
+								<h4 class="mt-40 mb-20">Commercial Service</h4>
+								<p>
+									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
+								</p>
+							</div>							
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End cat Area -->						
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+			<!-- Start project Area -->
+			<section class="project-area section-gap" id="project">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-md-8 pb-30 header-text text-center">
+							<h1 class="mb-10">Latest Finished Projects</h1>
+							<p>
+								Who are in extremely love with eco friendly system..
+							</p>
+						</div>
+					</div>						
+					<div class="row">
+						<div class="col-lg-8 col-md-8">
+							<a href="depan/img/p1.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="depan/img/p1.jpg" alt="">
+							</a>	
+						</div>
+						<div class="col-lg-4 col-md-4">
+							<a href="depan/img/p2.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="depan/img/p2.jpg" alt="">
+							</a>	
+						</div>						
+						<div class="col-lg-6 col-md-6">
+							<a href="depan/img/p3.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="depan/img/p3.jpg" alt="">
+							</a>	
+						</div>
+						<div class="col-lg-6 col-md-6">
+							<a href="depan/img/p4.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="depan/img/p4.jpg" alt="">
+							</a>	
+						</div>		
+					</div>
+				</div>	
+			</section>
+			<!-- End project Area -->
+			<!-- Start feedback Area -->
+			<section class="feedback-area aboutus-feedback section-gap" id="feedback">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-md-12 pb-60 header-text text-center">
+							<h1 class="mb-10">Enjoy our Client’s Feedback</h1>
+							<p>
+								Who are in extremely love with eco friendly system..
+							</p>
+						</div>
+					</div>			
+					<div class="row feedback-contents justify-content-center align-items-center">
+						<div class="col-lg-6 feedback-left relative d-flex justify-content-center align-items-center">
+							<div class="overlay overlay-bg"></div>
+							<a class="play-btn" href="https://www.youtube.com/watch?v=ARA0AxrnHdM"><img class="img-fluid" src="img/play-btn.png" alt=""></a>
+						</div>
+						<div class="col-lg-6 feedback-right">
+							<div class="active-review-carusel">
+								<div class="single-feedback-carusel">
+									<div class="title d-flex flex-row">
+										<h4 class="pb-10">Fannie Rowe</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>
+											<span class="fa fa-star"></span>								
+										</div>										
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+								</div>
+								<div class="single-feedback-carusel">
+									<div class="title d-flex flex-row">
+										<h4 class="pb-10">Fannie Rowe</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>								
+										</div>										
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+								</div>
+								<div class="single-feedback-carusel">
+									<div class="title d-flex flex-row">
+										<h4 class="pb-10">Fannie Rowe</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked	"></span>								
+										</div>										
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+								</div>																
+							</div>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End feedback Area -->
+				
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-5 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>About Us</h6>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+								</p>
+								<p class="footer-text">
+									<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								</p>								
+							</div>
+						</div>
+						<div class="col-lg-5  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Newsletter</h6>
+								<p>Stay update with our latest</p>
+								<div class="" id="mc_embed_signup">
+									<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
+										<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '" required="" type="email">
+			                            	<button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
+			                            	<div style="position: absolute; left: -5000px;">
+												<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+											</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+										<div class="info"></div>
+									</form>
+								</div>
+							</div>
+						</div>						
+						<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
+							<div class="single-footer-widget">
+								<h6>Follow Us</h6>
+								<p>Let us be social</p>
+								<div class="footer-social d-flex align-items-center">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-dribbble"></i></a>
+									<a href="#"><i class="fa fa-behance"></i></a>
+								</div>
+							</div>
+						</div>							
+					</div>
+				</div>
+			</footer>	
+			<!-- End footer Area -->	
 
-            .full-height {
-                height: 100vh;
-            }
+			<script src="depan/js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="depan/js/vendor/bootstrap.min.js"></script>			
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="depan/js/easing.min.js"></script>			
+			<script src="depan/js/hoverIntent.js"></script>
+			<script src="depan/js/superfish.min.js"></script>	
+			<script src="depan/js/jquery.ajaxchimp.min.js"></script>
+			<script src="depan/js/jquery.magnific-popup.min.js"></script>	
+			<script src="depan/js/owl.carousel.min.js"></script>	
+			<script src="depan/js/hexagons.min.js"></script>							
+			<script src="depan/js/jquery.nice-select.min.js"></script>	
+			<script src="depan/js/jquery.counterup.min.js"></script>
+			<script src="depan/js/waypoints.min.js"></script>							
+			<script src="depan/js/mail-script.js"></script>	
+			<script src="depan/js/main.js"></script>	
+		</body>
+	</html>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
