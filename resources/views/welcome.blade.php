0595e8b28cb2cce2be45be1c11c98b11ae64e442
tri angga utama <trianggategarutama@gmail.com>
2019-05-02 18:48:06 +0800

    ubah halaman depan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 		<!-- Site Title -->
 		<title>Dinas Perhubungan</title>
 
-		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
 			<!--
 			CSS
 			============================================= -->
@@ -24,13 +24,13 @@
 			<link rel="stylesheet" href="depan/css/font-awesome.min.css">
 			<link rel="stylesheet" href="depan/css/bootstrap.css">
 			<link rel="stylesheet" href="depan/css/magnific-popup.css">
-			<link rel="stylesheet" href="depan/css/nice-select.css">	
-			<link rel="stylesheet" href="depan/css/hexagons.min.css">							
+			<link rel="stylesheet" href="depan/css/nice-select.css">
+			<link rel="stylesheet" href="depan/css/hexagons.min.css">
 			<link rel="stylesheet" href="depan/css/animate.min.css">
 			<link rel="stylesheet" href="depan/css/owl.carousel.css">
 			<link rel="stylesheet" href="depan/css/main.css">
 		</head>
-		<body>	
+		<body>
 			  <header id="header" id="home">
 			    <div class="container main-menu">
 			    	<div class="row align-items-center justify-content-between d-flex">
@@ -39,12 +39,12 @@
 				      </div>
 				      <nav id="nav-menu-container">
 				        <ul class="nav-menu">
-				          <li class="menu-active"><a href="index.html">Home</a></li>
-				          <li><a href="about.html">About</a></li>
-				          <li><a href="services.html">Service</a></li>			          
-				          <li><a href="contact.html">Contact</a></li>
+				          <li class="menu-active"><a href="index.html">Beranda</a></li>
+				          <li><a href="about.html">Layanan Pelaporan</a></li>
+				          <li><a href="services.html">Macam macam layanan</a></li>
+				          <li><a href="contact.html">kontak</a></li>
 				        </ul>
-				      </nav><!-- #nav-menu-container -->		    		
+				      </nav><!-- #nav-menu-container -->
 			    	</div>
 			    </div>
 			  </header><!-- #header -->
@@ -55,12 +55,12 @@
 						<div class="banner-content col-lg-12 col-md-12">
                             <h5 class="text-uppercase" style="color:white;">Selamat Datang</h5>
 							<h1>
-								Aplikasi Pelaporan Kebutuhan dan Ketersediaan Rambu		
+								Aplikasi Pelaporan Kebutuhan dan Ketersediaan Rambu
 							</h1>
 							<p class="text-white">
                             Anda dapat memasukan laporan mengenai lokasi yang dirasa rawan dan dirasa memerluakn rambu lalu lintas untuk mengurangi adanya resiko terjadinya Laka Lalin
                         </p>
-						</div>												
+						</div>
 					</div>
 				</div>
 			</section>
@@ -70,12 +70,12 @@
 				<div class="container">
 					<div class="row justify-content-center align-items-center">
 						<div class="col-lg-8 col-md-12 home-about-left">
-							<h6>Brand new app to blow your mind</h6>
+							<h6>Laporan Langsung Masyarakat</h6>
 							<h1>
-								We’ve made a life <br>
-								that will change you
+								Anda Mempunyai Saran Maupun  <br>
+								Permintaan Rambu Lalulintas?
 							</h1>
-							<p class="sub">We are here to listen from you deliver exellence</p>
+							<p class="sub">Aplikasi ini Memudahkan Anda dalam melaporkan langsung pada pihak kami</p>
 							<p class="pb-20">
 								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
 							</p>
@@ -92,7 +92,7 @@
 										<option value="4">Service Three</option>
 										<option value="5">Service Four</option>
 									</select>
-								</div>								
+								</div>
 								<input type="text" class="form-control" placeholder="name">
 								<input type="phone" class="form-control" placeholder="Phone Number">
 								<input type="email" class="form-control" placeholder="Email Address">
@@ -101,80 +101,80 @@
 							</form>
 						</div>
 					</div>
-				</div>	
+				</div>
 			</section>
 			<!-- End home-about Area -->
-				
+
 			<!-- Start cat Area -->
 			<section class="cat-area section-gap aboutus-cat" id="feature">
-				<div class="container">							
+				<div class="container">
 					<div class="row">
-						<div class="col-lg-4">	
+						<div class="col-lg-4">
 							<div class="single-cat d-flex flex-column">
 								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-magic-wand"></span></span></a>
-								<h4 class="mb-20" style="margin-top: 23px;">Maintenance</h4>
+								<h4 class="mb-20" style="margin-top: 23px;">Rehabilitasi</h4>
 								<p>
-									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
+									Dinas Pehubungan mempunyai program rehabilitasi rambu lalu lintas dan lampu peringatan jalan
 								</p>
-							</div>															
+							</div>
 						</div>
-						<div class="col-lg-4">	
+						<div class="col-lg-4">
 							<div class="single-cat">
 								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-rocket"></span></span></a>
-								<h4 class="mt-40 mb-20">Residental Service</h4>
+								<h4 class="mt-40 mb-20">Pengadaan Rambu</h4>
 								<p>
-									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
+									Dians Perhubungan memiliki program pengadaan rambu lalu-lintas setiap tahunnya
 								</p>
-							</div>															
+							</div>
 						</div>
 						<div class="col-lg-4">
 							<div class="single-cat">
 								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-bug"></span></span></a>
-								<h4 class="mt-40 mb-20">Commercial Service</h4>
+								<h4 class="mt-40 mb-20">Pengadaan Sarpras Lalin </h4>
 								<p>
-									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
-								</p>
-							</div>							
+                                 Dinas Perhubungan juga memiliki program pengadaan sarana prasarana lalu lintas yang lain seperti lampu Peringatan,Cermin Cembung dan Lain-lain
+                                </p>
+							</div>
 						</div>
 					</div>
-				</div>	
+				</div>
 			</section>
-			<!-- End cat Area -->						
+			<!-- End cat Area -->
 
 			<!-- Start project Area -->
 			<section class="project-area section-gap" id="project">
 				<div class="container">
 					<div class="row justify-content-center">
 						<div class="col-md-8 pb-30 header-text text-center">
-							<h1 class="mb-10">Latest Finished Projects</h1>
+							<h1 class="mb-10">Foto Kegiatan</h1>
 							<p>
-								Who are in extremely love with eco friendly system..
+								para petugas survey langsung kelapangan untung mengetahui kondisi
 							</p>
 						</div>
-					</div>						
+					</div>
 					<div class="row">
 						<div class="col-lg-8 col-md-8">
-							<a href="depan/img/p1.jpg" class="img-gal">
-								<img class="img-fluid single-project" src="depan/img/p1.jpg" alt="">
-							</a>	
+							<a href="images/p1.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="images/p1.jpg" alt="">
+							</a>
 						</div>
 						<div class="col-lg-4 col-md-4">
-							<a href="depan/img/p2.jpg" class="img-gal">
-								<img class="img-fluid single-project" src="depan/img/p2.jpg" alt="">
-							</a>	
-						</div>						
-						<div class="col-lg-6 col-md-6">
-							<a href="depan/img/p3.jpg" class="img-gal">
-								<img class="img-fluid single-project" src="depan/img/p3.jpg" alt="">
-							</a>	
+							<a href="images/p2.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="images/p2.jpg" alt="">
+							</a>
 						</div>
 						<div class="col-lg-6 col-md-6">
-							<a href="depan/img/p4.jpg" class="img-gal">
-								<img class="img-fluid single-project" src="depan/img/p4.jpg" alt="">
-							</a>	
-						</div>		
+							<a href="images/p3.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="images/p3.jpg" alt="">
+							</a>
+						</div>
+						<div class="col-lg-6 col-md-6">
+							<a href="images/p4.jpg" class="img-gal">
+								<img class="img-fluid single-project" src="images/p4.jpg" alt="">
+							</a>
+						</div>
 					</div>
-				</div>	
+				</div>
 			</section>
 			<!-- End project Area -->
 			<!-- Start feedback Area -->
@@ -187,7 +187,7 @@
 								Who are in extremely love with eco friendly system..
 							</p>
 						</div>
-					</div>			
+					</div>
 					<div class="row feedback-contents justify-content-center align-items-center">
 						<div class="col-lg-6 feedback-left relative d-flex justify-content-center align-items-center">
 							<div class="overlay overlay-bg"></div>
@@ -203,8 +203,8 @@
 											<span class="fa fa-star checked"></span>
 											<span class="fa fa-star checked"></span>
 											<span class="fa fa-star"></span>
-											<span class="fa fa-star"></span>								
-										</div>										
+											<span class="fa fa-star"></span>
+										</div>
 									</div>
 									<p>
 										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
@@ -218,8 +218,8 @@
 											<span class="fa fa-star checked"></span>
 											<span class="fa fa-star checked"></span>
 											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star"></span>								
-										</div>										
+											<span class="fa fa-star"></span>
+										</div>
 									</div>
 									<p>
 										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
@@ -233,21 +233,21 @@
 											<span class="fa fa-star checked"></span>
 											<span class="fa fa-star checked"></span>
 											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked	"></span>								
-										</div>										
+											<span class="fa fa-star checked	"></span>
+										</div>
 									</div>
 									<p>
 										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
 									</p>
-								</div>																
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>	
+				</div>
 			</section>
 			<!-- End feedback Area -->
-				
-			<!-- start footer Area -->		
+
+			<!-- start footer Area -->
 			<footer class="footer-area section-gap">
 				<div class="container">
 					<div class="row">
@@ -261,7 +261,7 @@
 									<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-								</p>								
+								</p>
 							</div>
 						</div>
 						<div class="col-lg-5  col-md-6 col-sm-6">
@@ -280,7 +280,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 									</form>
 								</div>
 							</div>
-						</div>						
+						</div>
 						<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
 							<div class="single-footer-widget">
 								<h6>Follow Us</h6>
@@ -292,28 +292,28 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 									<a href="#"><i class="fa fa-behance"></i></a>
 								</div>
 							</div>
-						</div>							
+						</div>
 					</div>
 				</div>
-			</footer>	
-			<!-- End footer Area -->	
+			</footer>
+			<!-- End footer Area -->
 
 			<script src="depan/js/vendor/jquery-2.2.4.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-			<script src="depan/js/vendor/bootstrap.min.js"></script>			
+			<script src="depan/js/vendor/bootstrap.min.js"></script>
 			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-  			<script src="depan/js/easing.min.js"></script>			
+  			<script src="depan/js/easing.min.js"></script>
 			<script src="depan/js/hoverIntent.js"></script>
-			<script src="depan/js/superfish.min.js"></script>	
+			<script src="depan/js/superfish.min.js"></script>
 			<script src="depan/js/jquery.ajaxchimp.min.js"></script>
-			<script src="depan/js/jquery.magnific-popup.min.js"></script>	
-			<script src="depan/js/owl.carousel.min.js"></script>	
-			<script src="depan/js/hexagons.min.js"></script>							
-			<script src="depan/js/jquery.nice-select.min.js"></script>	
+			<script src="depan/js/jquery.magnific-popup.min.js"></script>
+			<script src="depan/js/owl.carousel.min.js"></script>
+			<script src="depan/js/hexagons.min.js"></script>
+			<script src="depan/js/jquery.nice-select.min.js"></script>
 			<script src="depan/js/jquery.counterup.min.js"></script>
-			<script src="depan/js/waypoints.min.js"></script>							
-			<script src="depan/js/mail-script.js"></script>	
-			<script src="depan/js/main.js"></script>	
+			<script src="depan/js/waypoints.min.js"></script>
+			<script src="depan/js/mail-script.js"></script>
+			<script src="depan/js/main.js"></script>
 		</body>
 	</html>
 
